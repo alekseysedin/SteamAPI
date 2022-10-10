c94844972af65d5a284facb5687225a8fa077d64
Aleksey Sedin <alekseysedin@outlook.com>
2022-10-10 16:35:58 +0400

    add

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
+     for (int i = 0; i < list.size(); i++) {
+         if (list.get(i) <= 0 || list.get(i) % 2 != 0) {
+             list.remove(i);
+             i--;
+         }
+     }
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+}
